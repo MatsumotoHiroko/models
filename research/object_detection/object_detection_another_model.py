e434487aa7173842097c5d10b667c0bcf38db13b
Hiroko Matsumoto <h.matsumoto@sint.co.jp>
2017-10-18 16:46:51 +0900

    チュートリアル外のモデル

---
+++ research/object_detection/object_detection_another_model.py
@@ -0,0 +1,70 @@
+# http://robotics4society.com/2017/08/23/odapi_test/
+# モジュールのインポート
+import numpy as np
+import sys
+import os
+import tensorflow as tf
+
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
+
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+
+
+# モデルの読み込み
+etection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  # モデルの読み込み．使用するモデルのパスを指定
+  with tf.gfile.GFile('ssd_inception_v2_coco_11_06_2017/frozen_inference_graph.pb', 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
+
+ # ラベルマップの読み込み
+label_map = label_map_util.load_labelmap('data/mscoco_label_map.pbtxt')
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=90, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+# ヘルパ関数の定義
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+
+# テスト画像の指定
+# テスト画像のディレクトリを指定．このディレクトリに任意の画像を入れておきます．
+PATH_TO_TEST_IMAGES_DIR = 'test_images'
+TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(1, 3) ]
+# 表示する画像のサイズを指定
+IMAGE_SIZE = (12, 8)
+
+# 検出と結果の表示
+with detection_graph.as_default():
+  with tf.Session(graph=detection_graph) as sess:
+    for image_path in TEST_IMAGE_PATHS:
+      image = Image.open(image_path)
+      image_np = load_image_into_numpy_array(image)
+      image_np_expanded = np.expand_dims(image_np, axis=0)
+      image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+      boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+      scores = detection_graph.get_tensor_by_name('detection_scores:0')
+      classes = detection_graph.get_tensor_by_name('detection_classes:0')
+      num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+      (boxes, scores, classes, num_detections) = sess.run(
+          [boxes, scores, classes, num_detections],
+          feed_dict={image_tensor: image_np_expanded})
+      vis_util.visualize_boxes_and_labels_on_image_array(
+          image_np,
+          np.squeeze(boxes),
+          np.squeeze(classes).astype(np.int32),
+          np.squeeze(scores),
+          category_index,
+          use_normalized_coordinates=True,
+          line_thickness=8)
+      plt.figure(figsize=IMAGE_SIZE)
+      plt.imshow(image_np)
+    plt.show()
