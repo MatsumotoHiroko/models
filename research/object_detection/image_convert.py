819184e224e258cb7e2388d8fa5b43bc9fd6746f
Ubuntu <siaitechlab@gmail.com>
2017-10-23 05:57:52 +0000

    image convert programm

--- research/object_detection/image_convert.py
+++ research/object_detection/image_convert.py
@@ -5,10 +5,10 @@ import config
 from PIL import Image
 
 output_ext = 'png'
-for dirpath, dirnames, filenames in os.walk(CROP_IMAGES_DIR):
+for dirpath, dirnames, filenames in os.walk(config.CROP_IMAGES_DIR):
     for dirname in dirnames:
         member_dir = os.path.join(dirpath, dirname)
-        convert_dir = os.path.join(PATH_TO_CROP_IMAGES_DIR, dirname)
+        convert_dir = os.path.join(config.CONVERT_IMAGES_DIR, dirname)
         print("##### member_dir")
         print(member_dir)
         print("##### convert_dir")
@@ -16,18 +16,20 @@ for dirpath, dirnames, filenames in os.walk(CROP_IMAGES_DIR):
         if not os.path.isdir(convert_dir):
             os.makedirs(convert_dir)
         for dirpath2, dirnames2, filenames2 in os.walk(member_dir):
-            if infile in filenames2:
+            for infile in filenames2:
                 (fn,ext) = os.path.splitext(infile)
                 outfile = fn + '.{}'.format(output_ext)
                 print("##### infile")
                 print(infile)
                 print("##### outfile")
                 print(outfile)
-                if ext.upper() in exts:
-                    im = Image.open(os.path.join(CROP_IMAGES_DIR, dirpath, infile)
+                if ext.upper() in config.exts:
+                    im = Image.open(os.path.join(dirpath, dirname, infile))
                     # image type convert        
                     if outfile != infile:
                         if im.mode != "RGB":
-                            im = im.convert("RGB")　# RGBモードに変換する
-                    im.resize(CONVERT_IMAGE_SIZE)        
-                    #im.save(os.path.join(convert_dir, outfile))        
+                            im = im.convert("RGB") # RGBモードに変換する
+                    im = im.resize(config.CONVERT_IMAGE_SIZE)        
+                    print("###### convert file")
+                    print(os.path.join(convert_dir, outfile))
+                    im.save(os.path.join(convert_dir, outfile))        
